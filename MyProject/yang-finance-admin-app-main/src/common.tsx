e9442d435c087ecdfa522076457e55f31ea59aaf
Gn84218 <95937108+Gn84218@users.noreply.github.com>
2024-12-31 14:03:08 +0800

    Delete MyProject directory

--- MyProject/yang-finance-admin-app-main/src/common.tsx
+++
@@ -1,7 +0,0 @@
-export function formatNumber(num: number) {
-  return num.toFixed(2);
-}
-
-export function formatAmount(amount: number) {
-  return `${amount}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-};
